04b1bcb7465014ba2f434c5418117c09bb3ccb37
dtroberts <droberts@wpi.edu>
2016-01-19 15:50:05 -0500

    Performed some refactoring and optimizations.

--- ZeroAdminTempHandler/src/zadmin/Main.java
+++ ZeroAdminTempHandler/src/zadmin/Main.java
@@ -1,23 +1,17 @@
 package zadmin;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
+import java.net.URLDecoder;
+import java.util.ArrayList;
 
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.transfer.TransferManager;
-import com.amazonaws.services.s3.transfer.Upload;
-import com.amazonaws.services.sns.AmazonSNSClient;
+import org.json.*;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 
 /** 
  * Used for on-the-fly testing.
@@ -31,119 +25,63 @@ public class Main {
 	static final String dbURL = "jdbc:redshift://dtroberts-ec2-test.c0y2ox1yoyin.us-west-2.redshift.amazonaws.com:5439/db";
 	static final String destBucket = "dtrobertstestbucket";
 	static final String destKey = "" + System.currentTimeMillis() + ".csv";
-	
+
 	public static void main(String[] args) {
-		// dedupe temp table against main table
-        Connection conn = null;
-		Statement stmt = null;
-		String sql, deduped = "";
-		ResultSet rs = null;
+		// Get information about the added file
+		S3Object rulesObject, s3Object = null;
+		InputStream rulesData, objectData = null;
+		String rulesKey = "rules.json";
+		ArrayList<String> columnNames = new ArrayList<String>();
+
+		try {
+			rulesKey = URLDecoder.decode(rulesKey, "UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		// Get the rules file
+		// Assumption: The rules file will always have the name "rules.json"
+		AmazonS3 s3Client = new AmazonS3Client();
+		rulesObject = s3Client.getObject(new GetObjectRequest("dtroberts-preprocess", "rules.json"));
+		rulesData = rulesObject.getObjectContent();
+		String rulesString = readInputStream(rulesData);
+		
+		JSONObject o = new JSONObject(rulesString);
+		JSONObject configurationObj = new JSONObject(o.getJSONObject("configuration").toString());
+		JSONObject schemaObj = new JSONObject(configurationObj.getJSONObject("schema").toString());
+		JSONArray fieldsArray = new JSONArray(schemaObj.getJSONArray("fields").toString());
+		
+		// Get column names
+		columnNames = new ArrayList<String>();
+		for (int i = 0; i < fieldsArray.length(); i++) {
+			// Do  something
+			String currentField = fieldsArray.getJSONObject(i).getString("name");
+			if (!currentField.substring(0, 3).equals("ob_")) {	// ignore "ob_" fields
+				columnNames.add(fieldsArray.getJSONObject(i).getString("name"));
+				System.out.println(columnNames.get(i));
+			}
+		}
+		System.out.println();
+	}
+
+	public static String readInputStream(InputStream stream) {
+		int i;
+		char c;
+		String result = "";
 		
 		try {
-			Class.forName("com.amazon.redshift.jdbc41.Driver");
-			Properties props = new Properties();
-			props.setProperty("user", masterUser);
-			props.setProperty("password", masterPassword);
-			conn = DriverManager.getConnection(dbURL, props);
-			
-			// retrieve unique rows
-			// TODO: Properly remove entries which are not placed in dedupe
-			stmt = conn.createStatement();
-			sql = "select * into temporary small_temp from temp limit " + MAX_PROCESSED;
-			stmt.execute(sql);
-			sql = "select * from small_temp except (select * from test)";
-			rs = stmt.executeQuery(sql);
-			
-			while (rs.next()) {
-				for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-					deduped += "" + rs.getString(i) + ",";
-				}
-				deduped = deduped.substring(0, deduped.length()-1);
-				deduped += "\r\n";
+			InputStreamReader isr = new InputStreamReader(stream);
+			StringBuilder sb = new StringBuilder();
+			while((i = isr.read()) != -1) {
+				// int to character
+				c= (char) i;
+				sb.append(c);
 			}
-			
-			// remove all processed entries from temp table
-			sql = "select * from small_temp";
-			rs = stmt.executeQuery(sql);
-			String processed = "";
-			int count = 0;
-			while (rs.next()) {
-				for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-					processed += "" + rs.getString(i) + ",";
-				}
-				processed = processed.substring(0, processed.length()-1);
-				processed += "\r\n";
-			}
-			stmt = updateTempTable(conn, processed, rs);
-			
-			// If empty, no new data needs to be written to the bucket
-			if (!deduped.isEmpty()) {	
-				writeDedupedToBucket(null, deduped);
-	        }
-			
-			// If num rows > MAX_PROCESSED, request a new function instance to process
-			if (rs.next()) {
-				stmt.close();
-				conn.close();
-				AmazonSNSClient snsClient = new AmazonSNSClient();
-				snsClient.setRegion(Region.getRegion(Regions.US_WEST_2));
-				//context.getLogger().log("New instance requested for further processing");
-				//snsClient.publish(destSNS, "run new dedupe");
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			result = sb.toString();
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
-    }
-
-    /**
-     * Update the temp table by removing the processed tuples.
-     * @param conn Connection to the database
-     * @param processed String containing the 
-     **/
-	private static Statement updateTempTable(Connection conn, String processed,
-			ResultSet rs) throws SQLException {
-		Statement stmt;
-		String sql;
-		String[] delete = processed.split("\r\n");
-		stmt = conn.createStatement();
-		sql = "delete from temp where (";
-		for (int i = 0; i < delete.length; i++) {
-			sql += "(";
-			String[] deleteRow = delete[i].split(",");
-			for (int j = 1; j <= rs.getMetaData().getColumnCount(); j++) {
-				sql += "(temp." + rs.getMetaData().getColumnName(j) + "=\'" + deleteRow[j-1] + "\')";
-				if (j != rs.getMetaData().getColumnCount()) {
-					sql += " and ";
-				}
-			}
-			sql += ") or ";
-		}
-		
-		if (!(sql.length() <= 25)) {	// protection against the case where there are no rows to clean up
-			sql = sql.substring(0, sql.length()-4);
-			sql += ")";
-			stmt.executeUpdate(sql);
-		}
-		return stmt;
-	}
-	
-	/** Write the deduped String as a CSV to the destination bucket.
-	 * @param context Context to write log to
-	 * @param deduped String to write to the bucket
-	 **/
-	private static void writeDedupedToBucket(Context context, String deduped)
-			throws UnsupportedEncodingException, InterruptedException {
-		TransferManager tx = new TransferManager();
-		InputStream is = new ByteArrayInputStream(deduped.getBytes("US-ASCII"));
-		ObjectMetadata metadata = new ObjectMetadata();
-		metadata.setContentType((String) "application/octet-stream");
-		metadata.setContentLength(deduped.length());
-		String destKey = "" + System.currentTimeMillis() + ".csv";
-		PutObjectRequest putRequest = new PutObjectRequest(destBucket, destKey, is, metadata);
-		Upload up = tx.upload(putRequest);
-		up.waitForCompletion();		// must block so that S3 does not get overloaded
-		context.getLogger().log("Successfully added " + destKey + " to " + destBucket);
-		context.getLogger().log(deduped);
+		return result;
 	}
 }
--- ZeroAdminTempHandler/src/zadmin/PreworkFunctionHandler.java
+++ ZeroAdminTempHandler/src/zadmin/PreworkFunctionHandler.java
@@ -1,10 +1,14 @@
 package zadmin;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -28,46 +32,93 @@ public class PreworkFunctionHandler implements RequestHandler<S3Event, Object> {
 	static final String dbURL = "jdbc:redshift://dtroberts-ec2-test.c0y2ox1yoyin.us-west-2.redshift.amazonaws.com:5439/db";
 	static final String destBucket = "dtrobertstestbucket";
 	static final String destSNS = "arn:aws:sns:us-west-2:602634635920:RedshiftSuccessfulLoads";
-	
-    @Override
-    /**
-     * This function performs a check against the rules file in this bucket.
-     * If the file is deemed valid, it is copied to the next bucket for Lambda processing.
-     * @param input S3 event which triggered this instance
-     * @param context Context used for logging
-     */
-    public Object handleRequest(S3Event input, Context context) {
-    	// Get information about the added file
-        S3EventNotificationRecord record = input.getRecords().get(0);
-        String srcBucket = record.getS3().getBucket().getName();
-        String srcKey = record.getS3().getObject().getKey().replace('+', ' ');
-        try {
+
+	@Override
+	/**
+	 * This function performs a check against the rules file in this bucket.
+	 * If the file is deemed valid, it is copied to the next bucket for Lambda processing.
+	 * @param input S3 event which triggered this instance
+	 * @param context Context used for logging
+	 */
+	public Object handleRequest(S3Event input, Context context) {
+		// Get information about the added file
+		S3Object rulesObject, s3Object = null;
+		InputStream rulesData, objectData = null;
+		ArrayList<String> columnNames;
+		S3EventNotificationRecord record = input.getRecords().get(0);
+		String srcBucket = record.getS3().getBucket().getName();
+		String srcKey = record.getS3().getObject().getKey().replace('+', ' ');
+
+		try {
 			srcKey = URLDecoder.decode(srcKey, "UTF-8");
 		} catch (UnsupportedEncodingException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-        
-        // Check file type
-        if (!srcKey.substring(srcKey.length()-4, srcKey.length()).equals(".csv")) {
-        	context.getLogger().log("ERROR: File is not .csv. The file will be ignored.");
-        	return null;
-        }
-        String destKey = srcKey;
-        
-        // Get the object
-        AmazonS3 s3Client = new AmazonS3Client();
-        S3Object s3Object = s3Client.getObject(new GetObjectRequest(srcBucket, srcKey));
-        InputStream objectData = s3Object.getObjectContent();
-        
-        // Retrieve the first line 
-        Scanner scan = new Scanner(objectData, "US-ASCII");
-        String dataString = scan.nextLine();
-        scan.close();
-    	
-    	// check formatting
-    	// check columns against rules file
-    	
-    	return null;
-    }
+
+		// Check file type
+		if (!srcKey.substring(srcKey.length()-4, srcKey.length()).equals(".csv")) {
+			context.getLogger().log("ERROR: File is not .csv. The file will be ignored.");
+			return null;
+		}
+		String destKey = srcKey;
+
+		// Get the rules file
+		// Assumption: The rules file will always have the name "rules.json"
+		AmazonS3 s3Client = new AmazonS3Client();
+		rulesObject = s3Client.getObject(new GetObjectRequest("dtroberts-preprocess", "rules.json"));
+		rulesData = rulesObject.getObjectContent();
+		String rulesString = readInputStream(rulesData);
+
+		JSONObject o = new JSONObject(rulesString);
+		JSONObject configurationObj = new JSONObject(o.getJSONObject("configuration").toString());
+		JSONObject schemaObj = new JSONObject(configurationObj.getJSONObject("schema").toString());
+		JSONArray fieldsArray = new JSONArray(schemaObj.getJSONArray("fields").toString());
+
+		// Get column names
+		columnNames = new ArrayList<String>();
+		for (int i = 0; i < fieldsArray.length(); i++) {
+			// Do  something
+			String currentField = fieldsArray.getJSONObject(i).getString("name");
+			if (!currentField.substring(0, 3).equals("ob_")) {	// ignore "ob_" fields
+				columnNames.add(fieldsArray.getJSONObject(i).getString("name"));
+				System.out.println(columnNames.get(i));
+			}
+		}
+
+		// Get the object
+		s3Object = s3Client.getObject(new GetObjectRequest(srcBucket, srcKey));
+		objectData = s3Object.getObjectContent();
+
+		// Retrieve the first line of the new .csv object
+		Scanner scan = new Scanner(objectData, "US-ASCII");
+		String dataString = scan.nextLine();
+		scan.close();
+
+		// TODO: Load the temp table in the order specified by the rules file
+		// - reorder the CSV file itself?
+		//		- this allows us to leverage the usefulness of the zero-admin loader
+		// - copy into the temp table manually?
+
+		return null;
+	}
+
+	public static String readInputStream(InputStream stream) {
+		int i;
+		char c;
+		String result = "";
+
+		try {
+			InputStreamReader isr = new InputStreamReader(stream);
+			StringBuilder sb = new StringBuilder();
+			while((i = isr.read()) != -1) {
+				// int to character
+				c= (char) i;
+				sb.append(c);
+			}
+			result = sb.toString();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
